e964e82b7532bb9b526028a56ac8346a9f46e56d
pradeepmotyar <pradeepmotyar@gmail.com>
2020-11-02 21:41:24 -0800

    array-operators

---
+++ array-operator/array-operator.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = array("a" =>"red" , "b" => "yellow");
+$y = array("c" => "blue" , "d"=>"green");
+print_r($x + $y) ;
+//union of $x and $y   
+?>  
+
+</body>
+</html>
