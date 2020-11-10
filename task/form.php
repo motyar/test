346154c7ca65947b2fdaec8b2db9b49909652131
pradeepmotyar <pradeepmotyar@gmail.com>
2020-11-10 13:46:59 -0800

    Insert row using form

---
+++ task/form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Inserting data into database using php</title>
+</head>
+<body>
+<form action="insert.php" method="post">
+<table>
+<tr>
+<td>Name</td>
+<td><input type="text" name="name">
+</tr>
+<tr>
+<td><input type="submit" name="submit" value="Add"</td>
+</table>
+
+</form>
+</body>
+</html>
---
+++ task/insert.php
@@ -0,0 +1,12 @@
+<?php
+
+$name = $_POST['name'];
+$dbconnect= mysqli_connect('localhost','root','','test');
+ $sql = mysqli_query($dbconnect, "insert into students(id , name) values('','$name')");
+ if($sql){
+	 echo "data inserted successfully";
+ }
+ else{
+	 echo "Failed to insert";
+ }
+?>
