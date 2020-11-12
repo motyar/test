e9bb95277cdf3c2123fe43aefae53dfe657742a9
pradeepmotyar <pradeepmotyar@gmail.com>
2020-11-11 18:12:19 -0800

    form value shows in table

--- task/form.php
+++ task/form.php
@@ -4,16 +4,41 @@
 <title>Inserting data into database using php</title>
 </head>
 <body>
-<form action="insert.php" method="post">
-<table>
+<?php
+
+$name =$_POST['name'];
+$dbconnect= mysqli_connect('localhost','root','','test');
+ $sql = mysqli_query($dbconnect, "insert into students(id , name) values('','$name')");
+ 
+ if($sql){
+	 echo "data inserted successfully";
+ }
+ else{
+	 echo "Failed to insert";
+ }
+
+$result =mysqli_query($dbconnection, "SELECT * FROM students");
+echo"<table borde=1px>
+ 
 <tr>
+<td><form action=""  method ="POST"></td>
 <td>Name</td>
-<td><input type="text" name="name">
+<td><input type="text" name="name" ></td>
 </tr>
 <tr>
-<td><input type="submit" name="submit" value="Add"</td>
+<td><input type="submit" name="submit" value="Add"></td>
+</tr>;
+while($row = mysqli_fetch_array($result))
+{
+	echo "<tr>";
+	echo "<td>" . $row['Name']."</td>';
+	echo "</tr>";
+}
+echo"</table>";
+mysqli_close($con);
 </table>
 
 </form>
+?>
 </body>
 </html>
--- task/insert.php
+++ task/insert.php
@@ -1,12 +1,14 @@
 <?php
 
-$name = $_POST['name'];
+$name =$_POST['name'];
 $dbconnect= mysqli_connect('localhost','root','','test');
  $sql = mysqli_query($dbconnect, "insert into students(id , name) values('','$name')");
+ 
  if($sql){
 	 echo "data inserted successfully";
  }
  else{
 	 echo "Failed to insert";
  }
+
 ?>
