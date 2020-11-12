7d4068b390184f7c34f9201c5a1e610aca040905
pradeepmotyar <pradeepmotyar@gmail.com>
2020-11-12 15:32:35 -0800

    json decode and encode

--- task/upload-file.php
+++ task/upload-file.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,7 +5,6 @@
 </head>
 <body>
 <form action="action.php" method="POST" enctype="multipart/form-data">
-     
     <p><input type ="file" name="myfile"></p>
 	<p><input type="submit" name="upload" value="SEND"></p>
 </form>
