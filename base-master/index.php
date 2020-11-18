c000eb6b359be6baf99038457f41868f496f7a9a
pradeepmotyar <pradeepmotyar@gmail.com>
2020-11-18 11:49:19 -0800

    change index

--- base-master/index.php
+++ base-master/index.php
@@ -1,5 +1,5 @@
 <h1>
 	Welcome 
 </h1>
-<a href="login.php"><button>Login</button></a>
-<a href="register.php"><button>Register</button></a>
+<a href="signin.php"><button>Sign in</button></a>
+<a href="singup.php"><button>sign up</button></a>
