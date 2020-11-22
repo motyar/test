741fc5104034e2eec8579b9615f2be49e2b12d75
pradeepmotyar <pradeepmotyar@gmail.com>
2020-11-22 10:06:47 -0800

    add city

--- base-master/register.php
+++ base-master/register.php
@@ -10,7 +10,8 @@ if (isset($register)) {
     $password = md5($password);
     $rows = $db->Q("SELECT * FROM `form` WHERE `email`= '$email'");
     if (! $rows) {
-        $db->Q("INSERT INTO `form`(`email`,`password`) VALUES ('$email','$password')");
+	    // step 4
+        $db->Q("INSERT INTO `form`(`email`,`password`,'city') VALUES ('$email','$password','$city')");
         $message = '<h1>You are successfully registered plese click <a href="login.php">here</a> to login';
     } else {
         $message = 'sorry this email is already taken';
@@ -23,7 +24,10 @@ if (isset($register)) {
 	<input type="email" placeholder="email" name="email" required><br><br>
 	<label for="password">Password:</label>
 	<input type="password" placeholder="password" name="password" required><br><br>
-	<input type ="city" placeholder="city" name="city" required><br><br>
+	<label for ="citys"><b>city</b></label><br>
+	<select name="citys" id="citys" style="width:500px">
+	<option value ="value">city1</option>
+	<option value="value">city</option></select>
 	<input type="submit" name="register" value="Register">
 </form>
 <a href="login.php"><button>Login</button></a>
